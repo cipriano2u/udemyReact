80462b900b19a8e494fdf864b955fdb464da4220
Cipriano Mauricio <ciprianomauricio@Ciprianos-MacBook-Pro.local>
2019-05-21 09:43:23 +0200

    Updates

---
+++ precache-manifest.0656a58946ca2ed6ff9232cd15774947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerBuilder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "86fb03ad78e40fa8af7c",
+    "url": "/burgerBuilder/static/js/runtime~main.86fb03ad.js"
+  },
+  {
+    "revision": "477e59eda2f943f9d2fb",
+    "url": "/burgerBuilder/static/js/main.477e59ed.chunk.js"
+  },
+  {
+    "revision": "e6baad739309c3769aef",
+    "url": "/burgerBuilder/static/js/2.e6baad73.chunk.js"
+  },
+  {
+    "revision": "477e59eda2f943f9d2fb",
+    "url": "/burgerBuilder/static/css/main.2719d6bc.chunk.css"
+  },
+  {
+    "revision": "9c83870182b48e05445f88011ffd5d30",
+    "url": "/burgerBuilder/index.html"
+  }
+];
